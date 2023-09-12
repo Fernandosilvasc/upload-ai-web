5267e6c0f21367ed4238c77403b89812e20c423d
Fernandosilvasc <fernando_silvasc@hotmail.com>
2023-09-12 10:11:02 -0700

    feat: :sparkles: Add the Dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import {Button} from "./components/ui/button";
+import { ThemeProvider } from "@/components/theme-provider"
+import { ModeToggle } from "@/components/mode-toggle"
+import { Button } from "./components/ui/button"
 
 export function App() {
-
   return (
-    <>
-      <h1>UPLOAD AI</h1>
-      <Button>Click here</Button>
-    </>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <>
+        <header className='w-full'>
+          <ModeToggle />
+        </header>
+
+        <div>
+          <Button> Click Here</Button>
+        </div>
+      </>
+    </ThemeProvider>
   )
 }
 
+export default App
