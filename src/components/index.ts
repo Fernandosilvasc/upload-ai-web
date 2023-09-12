ad91f2b39ca858d1ebaaaaaf4599b2175b47b23b
Fernandosilvasc <fernando_silvasc@hotmail.com>
2023-09-12 14:07:14 -0700

    feat: :construction: Add components Label, Select, Separator, Slider and TextArea from shadcnui provider

---
+++ src/components/index.ts
@@ -0,0 +1,21 @@
+import { ModeToggle } from '@/components/mode-toggle';
+import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
+import { Textarea } from '@/components/ui/textarea';
+import { Label } from '@/components/ui/label';
+import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from '@/components/ui/select';
+import { Slider } from '@/components/ui/slider';
+
+export {
+  ModeToggle,
+  Button,
+  Separator,
+  Textarea,
+  Label,
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectItem,
+  Slider
+}
